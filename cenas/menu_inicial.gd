06706d3c487094086080f18f105e72b76ea24f65
benhuk <felipebenhuk55@gmail.com>
2025-10-01 10:31:57 -0300

    botao de volume configurado + musica

--- cenas/menu_inicial.gd
+++ cenas/menu_inicial.gd
@@ -5,3 +5,7 @@ func _on_jogar_pressed() -> void:
 	print("botao jogar pressionado")
 	get_tree().change_scene_to_file("res://cenas/menu-selecao-fase.tscn")
 	
+
+
+func _on_sair_pressed() -> void:
+	get_tree().quit()
