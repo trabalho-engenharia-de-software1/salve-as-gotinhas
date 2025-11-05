dabb06ef28ccb644e8fccfcd49f7299fba8dcead
Tobias <118182703+rochatobias@users.noreply.github.com>
2025-11-05 17:00:06 -0300

    Atualiza main com conteúdo de nova-felipeR (sem histórico)

--- scripts/menu-inicial/menu_inicial.gd
+++ scripts/menu-inicial/menu_inicial.gd
@@ -45,3 +45,7 @@ func _on_jogar_pressed() -> void:
 
 func _on_sair_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_creditos_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/menu-inicial/creditos.tscn")
